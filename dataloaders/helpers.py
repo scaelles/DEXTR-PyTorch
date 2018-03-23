4192c015f70fc50238ae4371d7ee296329e080e6
kmaninis <kmaninis@vision.ee.ethz.ch>
2018-03-23 14:19:42 +0100

    demo updated, gif updated

--- dataloaders/helpers.py
+++ dataloaders/helpers.py
@@ -70,14 +70,15 @@ def crop2fullmask(crop_mask, bbox, im=None, im_size=None, zero_pad=False, relax=
 
 def overlay_mask(im, ma, colors=None, alpha=0.5):
     assert np.max(im) <= 1.0
-    if not colors:
+    if colors is None:
         colors = np.load(os.path.join(os.path.dirname(__file__), 'pascal_map.npy'))/255.
+    else:
+        colors = np.append([[0.,0.,0.]], colors, axis=0);
+
     if ma.ndim == 3:
         assert len(colors) >= ma.shape[0], 'Not enough colors'
     ma = ma.astype(np.bool)
     im = im.astype(np.float32)
-
-    # fg    = im*alpha + np.ones(im.shape)*(1-alpha) * np.array([23,23,197])/255.0
 
     if ma.ndim == 2:
         fg = im * alpha+np.ones(im.shape) * (1 - alpha) * colors[1, :3]   # np.array([0,0,255])/255.0
@@ -105,6 +106,32 @@ def overlay_mask(im, ma, colors=None, alpha=0.5):
     cv2.drawContours(bg, contours[0], -1, (0.0, 0.0, 0.0), 1)
 
     return bg
+
+def overlay_masks(im, masks, alpha=0.5):
+    colors = np.load(os.path.join(os.path.dirname(__file__), 'pascal_map.npy'))/255.
+    
+    if isinstance(masks, np.ndarray):
+        masks = [masks]
+
+    assert len(colors) >= len(masks), 'Not enough colors'
+
+    ov = im.copy()
+    im = im.astype(np.float32)
+    total_ma = np.zeros([im.shape[0], im.shape[1]])
+    i = 1
+    for ma in masks:
+        ma = ma.astype(np.bool)
+        fg = im * alpha+np.ones(im.shape) * (1 - alpha) * colors[i, :3]   # np.array([0,0,255])/255.0
+        i = i + 1
+        ov[ma == 1] = fg[ma == 1]
+        total_ma += ma
+
+        # [-2:] is s trick to be compatible both with opencv 2 and 3
+        contours = cv2.findContours(ma.copy().astype(np.uint8), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[-2:]
+        cv2.drawContours(ov, contours[0], -1, (0.0, 0.0, 0.0), 1)
+    ov[total_ma == 0] = im[total_ma == 0]
+
+    return ov
 
 
 def extreme_points(mask, pert):
--- demo.py
+++ demo.py
@@ -4,7 +4,6 @@ from PIL import Image
 import numpy as np
 from matplotlib import pyplot as plt
 
-
 from torch.nn.functional import upsample
 from torch.autograd import Variable
 
@@ -15,7 +14,7 @@ from dataloaders import helpers as helpers
 modelName = 'dextr_pascal-sbd'
 pad = 50
 thres = 0.8
-gpu_id = 1
+gpu_id = 0
 
 #  Create the network and load the weights
 net = resnet.resnet101(1, nInputChannels=4, classifier='psp')
@@ -32,41 +31,46 @@ image = np.array(Image.open('ims/dog-cat.jpg'))
 plt.ion()
 plt.axis('off')
 plt.imshow(image)
-plt.title('Click the four extreme points')
-extreme_points_ori = np.array(plt.ginput(4, timeout=0)).astype(np.int)
+plt.title('Click the four extreme points of the objects\nHit enter when done (do not close the window)')
 
-#  Crop image to the bounding box from the extreme points and resize
-bbox = helpers.get_bbox(image, points=extreme_points_ori, pad=pad, zero_pad=True)
-crop_image = helpers.crop_from_bbox(image, bbox, zero_pad=True)
-resize_image = helpers.fixed_resize(crop_image, (512, 512)).astype(np.float32)
+results = []
 
-#  Generate extreme point heat map normalized to image values
-extreme_points = extreme_points_ori - [np.min(extreme_points_ori[:, 0]), np.min(extreme_points_ori[:, 1])] + [pad, pad]
-extreme_points = (512*extreme_points*[1/crop_image.shape[1], 1/crop_image.shape[0]]).astype(np.int)
-extreme_heatmap = helpers.make_gt(resize_image, extreme_points, sigma=10)
-extreme_heatmap = helpers.cstm_normalize(extreme_heatmap, 255)
+while 1:
+    extreme_points_ori = np.array(plt.ginput(4, timeout=0)).astype(np.int)
 
-#  Concatenate inputs and convert to tensor
-input_dextr = np.concatenate((resize_image, extreme_heatmap[:, :, np.newaxis]), axis=2)
-input_dextr = torch.from_numpy(input_dextr.transpose((2, 0, 1))[np.newaxis, ...])
+    #  Crop image to the bounding box from the extreme points and resize
+    bbox = helpers.get_bbox(image, points=extreme_points_ori, pad=pad, zero_pad=True)
+    crop_image = helpers.crop_from_bbox(image, bbox, zero_pad=True)
+    resize_image = helpers.fixed_resize(crop_image, (512, 512)).astype(np.float32)
 
-# Run a forward pass
-inputs = Variable(input_dextr, volatile=True)
-if gpu_id >= 0:
-    inputs = inputs.cuda()
+    #  Generate extreme point heat map normalized to image values
+    extreme_points = extreme_points_ori - [np.min(extreme_points_ori[:, 0]), np.min(extreme_points_ori[:, 1])] + [pad,
+                                                                                                                  pad]
+    extreme_points = (512 * extreme_points * [1 / crop_image.shape[1], 1 / crop_image.shape[0]]).astype(np.int)
+    extreme_heatmap = helpers.make_gt(resize_image, extreme_points, sigma=10)
+    extreme_heatmap = helpers.cstm_normalize(extreme_heatmap, 255)
 
-outputs = net.forward(inputs)
-outputs = upsample(outputs, size=(512, 512), mode='bilinear')
-if gpu_id >= 0:
-    outputs = outputs.cpu()
+    #  Concatenate inputs and convert to tensor
+    input_dextr = np.concatenate((resize_image, extreme_heatmap[:, :, np.newaxis]), axis=2)
+    input_dextr = torch.from_numpy(input_dextr.transpose((2, 0, 1))[np.newaxis, ...])
 
-pred = np.transpose(outputs.data.numpy()[0, ...], (1, 2, 0))
-pred = 1 / (1 + np.exp(-pred))
-pred = np.squeeze(pred)
-result = helpers.crop2fullmask(pred, bbox, im_size=image.shape[:2], zero_pad=True, relax=pad) > thres
+    # Run a forward pass
+    inputs = Variable(input_dextr, volatile=True)
+    if gpu_id >= 0:
+        inputs = inputs.cuda()
 
-# Plot the results
-plt.imshow(helpers.overlay_mask(image/255, result))
-plt.plot(extreme_points_ori[:, 0], extreme_points_ori[:, 1], 'gx')
-plt.ioff()
-plt.show()
+    outputs = net.forward(inputs)
+    outputs = upsample(outputs, size=(512, 512), mode='bilinear')
+    if gpu_id >= 0:
+        outputs = outputs.cpu()
+
+    pred = np.transpose(outputs.data.numpy()[0, ...], (1, 2, 0))
+    pred = 1 / (1 + np.exp(-pred))
+    pred = np.squeeze(pred)
+    result = helpers.crop2fullmask(pred, bbox, im_size=image.shape[:2], zero_pad=True, relax=pad) > thres
+
+    results.append(result)
+
+    # Plot the results
+    plt.imshow(helpers.overlay_masks(image / 255, results))
+    plt.plot(extreme_points_ori[:, 0], extreme_points_ori[:, 1], 'gx')
